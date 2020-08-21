b0d21f909525fa1c9015f7acf5ffe352ce102965
AAYURSHI <42316239+AAYURSHI@users.noreply.github.com>
2020-08-21 15:13:40 +0530

    Given path of messages

--- src/main/java/com/aayurshi/musical/MusicalApplication.java
+++ src/main/java/com/aayurshi/musical/MusicalApplication.java
@@ -1,22 +1,24 @@
 package com.aayurshi.musical;
 
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class MusicalApplication implements CommandLineRunner{
-
+@PropertySource(value={"classpath:messages.properties"})
+public class MusicalApplication {
+//implements CommandLineRunner
 	public static void main(String[] args) {
 		SpringApplication.run(MusicalApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Me Panda");
-		
-		
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		// TODO Auto-generated method stub
+//		System.out.println("Hello");
+//		
+//		
+//	}
 
 }
